e12bf016372946ad318762daba7626b894ef72bd
Tanvir <Tanvirislamnyc@gmail.com>
2024-10-02 19:00:27 -0400

    finished Card components

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,84 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
-
-
+  const [count, setCount] = useState(1);
   const [flip, setFlip] = useState(false);
   
 
   const Prophets = [
-    { id:1, question: "Which prophet is known for his steadfastness and patience during trials and tribulations?", answer: "Ayyub (Job)" },
-    { id: 2, question: "Who is the prophet known for building the Kaaba in Mecca?", answer: "Ibrahim (Abraham)" },
-    { id: 3, question: "Which prophet was known for his wisdom and ruled over the Israelites?", answer: "Dawud (David)" },
-    { id: 4, question: "Who is the prophet that led the Children of Israel out of Egypt?", answer: "Musa (Moses)" },
-    { id: 5, question: "Which prophet is associated with the story of the Ark and the Great Flood?", answer: "Nuh (Noah)" },
-    { id: 6, question: "Who is the prophet known for his miraculous birth and ability to speak in the cradle?", answer: "Isa (Jesus)" },
-    { id: 7, question: "Which prophet was swallowed by a big fish or whale and repented while inside?", answer: "Yunus (Jonah)" },
-    { id: 8, question: "Who is considered the final prophet in Islam, known as the Seal of the Prophets?", answer: "Muhammad" },
-    { id:9, question: "Which prophet is known for receiving the Psalms (Zabur)?", answer: "Dawud (David)" },
-    { id:10, question: "Who is the prophet known for his eloquent speeches and receiving the scriptures (Injil)?", answer: "Isa (Jesus)" }
-  ];
-  
-  return (
-    <>
-      <h1>Knowledge on Islamic Prophets</h1>
-      <h3>How well do you know the Islamic prophets?</h3>
-      <div className="container">
-        {Prophets.map((prophet, index) => (
-          <Card key={index} question={prophet.question} answer={prophet.answer} flip={flip} setFlip={setFlip} />
-        ))}
-        </div>
-    </>
-  )
+    { 
+      id: 1,
+      question: "Which prophet is known for his steadfastness and patience during trials and tribulations?", 
+      answer: "Ayyub (Job)" 
+    },
+    { 
+      id: 2, 
+      question: "Who is the prophet known for building the Kaaba in Mecca?", 
+      answer: "Ibrahim (Abraham)" 
+    },
+    { 
+      id: 3, 
+      question: "Which prophet was known for his wisdom and ruled over the Israelites?", 
+      answer: "Dawud (David)" 
+    },
+    { 
+      id: 4, 
+      question: "Who is the prophet that led the Children of Israel out of Egypt?", 
+      answer: "Musa (Moses)" 
+    },
+    { 
+      id: 5, 
+      question: "Which prophet is associated with the story of the Ark and the Great Flood?", 
+      answer: "Nuh (Noah)" 
+    },
+    { 
+      id: 6, 
+      question: "Who is the prophet known for his miraculous birth and ability to speak in the cradle?", 
+      answer: "Isa (Jesus)" 
+    },
+    { 
+      id: 7, 
+      question: "Which prophet was swallowed by a big fish or whale and repented while inside?", 
+      answer: "Yunus (Jonah)" 
+    },
+    { 
+      id: 8, 
+      question: "Who is considered the final prophet in Islam, known as the Seal of the Prophets?", 
+      answer: "Muhammad" 
+    },
+    { 
+      id: 9, 
+      question: "Which prophet is known for receiving the Psalms (Zabur)?", 
+      answer: "Dawud (David)" 
+    },
+    { 
+      id: 10, 
+      question: "Who is the prophet known for his eloquent speeches and receiving the scriptures (Injil)?", 
+      answer: "Isa (Jesus)" 
+    }
+];
+
+const Back = () => {
+  if (count > 0) {
+    setCount(count - 1);
+    setFlip(false)
+  }
+}
+
+const Next = () => {
+  if (count < Prophets.length - 1) {
+    setCount(count + 1);
+    setFlip(false)
+  }
+}
+
+return (
+  <>
+    <div className="bg-[url('https://i.etsystatic.com/38660474/r/il/12429e/4352455753/il_fullxfull.4352455753_jinn.jpg')] bg-cover bg-center fixed inset-0 flex flex-col justify-center items-center text-center min-h-screen">
+        
+    </div>
+  </>
+)
 }
 
 export default App
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,32 @@
 
 import React from 'react';
 
+
+
 interface CardProps {
+  id: string;
   question: string;
   answer: string;
-  flip: boolean;
+  flipped: boolean;
   setFlip: (flip: boolean) => void;
 }
 
-const Card: React.FC<CardProps> = ({ question, answer, flip, setFlip }) => {
-
-  const handleFlip = () => {
-    setFlip(!flip);
-  }
+const Card = (props: CardProps) => {
   
+  const flip = () => {
+    props.setFlip(!props.flipped);
+  }
+
   return (
-    <div onClick={handleFlip}>
-      <h3>{question}</h3>
-      <p>{answer}</p>
-      
+    <div>
+      <div 
+            className="flex flex-col items-center justify-center bg-white border rounded-lg shadow-lg p-10 w-full max-w-2xl h-80 cursor-pointer transition-transform transform hover:scale-105" onClick={flip}>
+            <p className="font-bold font-serif text-4xl text-blue-500 mb-4">{props.id}</p>
+            {props.flipped ? (<p className="text-2xl text-center font-serif">{props.answer}</p>) : (<p className="text-2xl text-center font-serif">{props.question}</p>)}
+        </div>
     </div>
   )
+   
 }
 
 export default Card;
